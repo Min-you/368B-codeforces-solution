aefd90562143df2ea211128460bc3f8b71edf0b8
Min-you <101444459+Min-you@users.noreply.github.com>
2022-09-17 16:00:35 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main() {
+    ll n , m;
+    cin >> n >> m;
+    ll a[n] , b[n];
+    set<ll> s;
+    for(ll i = 0 ; i < n ;i++)
+    {
+        cin >> a[i];
+    }
+    for(ll i = n - 1 ; i >= 0 ; i--)
+    {
+        s.insert(a[i]);
+        b[i]=s.size();
+    }
+    ll x;
+    for(ll i = 0 ; i < m ; i++)
+    {
+        cin >> x;
+        x--;
+        cout << b[x] << endl;
+    }
+    return 0;
+}
